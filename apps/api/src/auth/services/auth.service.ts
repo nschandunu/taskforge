523fea94cf156c46863a2e391db2aef6f605cde5
Senuka Chandunu <senukaacc@gmail.com>
2026-07-16 09:19:32 +0530

    feat(auth): implement user authentication and database seeding

--- apps/api/src/auth/services/auth.service.ts
+++ apps/api/src/auth/services/auth.service.ts
@@ -1,14 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
+import { PrismaService } from '../../prisma/prisma.service';
 import { LoginDto } from '../dto/login.dto';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
+
   async login(loginDto: LoginDto) {
-    console.log(loginDto);
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: loginDto.email,
+      },
+      include: {
+        role: true,
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const passwordMatched = await bcrypt.compare(
+      loginDto.password,
+      user.password,
+    );
+
+    if (!passwordMatched) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
 
     return {
-      message: 'Login endpoint working.',
+      message: 'Credentials verified.',
+      user,
     };
   }
 }

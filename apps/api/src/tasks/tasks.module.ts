1335a3ecc503b4dabf5d08da46b321bd16334cd0
Senuka Chandunu <senukaacc@gmail.com>
2026-07-16 10:36:42 +0530

    feat(tasks): implement core task management

---
+++ apps/api/src/tasks/tasks.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+
+import { PrismaModule } from '../prisma/prisma.module';
+
+import { TasksController } from './controllers/tasks.controller';
+import { TasksService } from './services/tasks.service';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [TasksController],
+  providers: [TasksService],
+  exports: [TasksService],
+})
+export class TasksModule {}
